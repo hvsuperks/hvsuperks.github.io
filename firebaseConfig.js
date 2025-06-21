78796559fbf522edf57af6243721e25867c2dd6e
hvsuperks <144697386+hvsuperks@users.noreply.github.com>
2025-06-21 19:22:00 +0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 {
-  apiKey: "AIzaSyAOOtP5Va7zCa9lH62H20piGYCvdACt9DE",
-  authDomain: "be-ca-ad623.firebaseapp.com",
+apiKey: "AIzaSyAOOtP5Va7zCa9lH62H20piGYCvdACt9DE",
+  authDomain: "be-ca-ad623.firebaseapp.com", // ✅ đúng: dùng dấu ":"
   databaseURL: "https://be-ca-ad623-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "be-ca-ad623",
   storageBucket: "be-ca-ad623.firebasestorage.app",

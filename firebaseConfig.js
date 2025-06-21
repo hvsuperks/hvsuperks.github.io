55c7a1f7aab5ad3ba4c6720041515cb75ae30d59
hvsuperks <144697386+hvsuperks@users.noreply.github.com>
2025-06-21 19:28:24 +0700

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-{
-apiKey: "AIzaSyAOOtP5Va7zCa9lH62H20piGYCvdACt9DE",
-  authDomain: "be-ca-ad623.firebaseapp.com", // ✅ đúng: dùng dấu ":"
-  databaseURL: "https://be-ca-ad623-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "be-ca-ad623",
-  storageBucket: "be-ca-ad623.firebasestorage.app",
-  messagingSenderId: "648232770855",
-  appId: "1:648232770855:web:8457f90c7a5bc0f5a3dd02"
-}
